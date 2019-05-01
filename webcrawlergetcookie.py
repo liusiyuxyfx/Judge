c7526e8857372195ecf057485833a72d52050fae
liusiyuxyfx <liusiyuxyfx@163.com>
2019-05-01 21:57:03 +0800

    10.00

--- webcrawlergetcookie.py
+++ webcrawlergetcookie.py
@@ -1,11 +1,13 @@
 #参考
 #https://blog.csdn.net/weixin_40444270/article/details/80593058
 #https://blog.csdn.net/KaryKwok/article/details/80943843
+#defaultdict使用 https://www.cnblogs.com/jidongdeatao/p/6930325.html
 import json
 import string
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from collections import defaultdict
 import time
 from lxml import etree
 
@@ -16,7 +18,7 @@ with open('mooccookie.json','r',encoding='utf-8') as f:
 url = 'https://www.icourse163.org/spoc/learn/COMPUTER-1002604037?tid=1002792051&_trace_c_p_k2_=7fbdf498494f4412bd9f3cc97483253d#/learn/forumdetail?pid=1005195052'
 
 #初始化数据
-answerdict={}
+answerdict = defaultdict(list)
 
 #注入cookie
 driver = webdriver.Chrome()
@@ -26,17 +28,24 @@ for item in listCookies: driver.add_cookie(item)
 
 #首次获取页面
 driver.get(url)
-time.sleep(1)
+time.sleep(2)
 divs = driver.find_elements_by_xpath('//*[@id="courseLearn-inner-box"]/div/div[2]/div/div[4]/div/div[1]/div[1]/*')
-
+pages = driver.find_elements_by_xpath('//*[@id="courseLearn-inner-box"]/div/div[2]/div/div[4]/div/div[1]/div[2]/div[1]/*')
+print(pages[-2].get_attribute("innerText"))
 #print(divs)
 for div in divs:
     answer = ""
     student = ""
     #print(div.get_attribute("innerHTML"))
     answercontent = div.find_element_by_xpath('.//div[contains(@class, "f-richEditorText j-content")]')
-    answer = print(answercontent.get_attribute("innerText"))
+    answer = answercontent.get_attribute("innerText")
     usercontent = div.find_element_by_xpath('.//span[@class="userInfo j-userInfo"]/a')
     student = usercontent.get_attribute("title")
-    if answer != None:
-        print ("%s : %s \n" % (student, answer))
+
+    if answer != None and student != None:
+        answerdict[student].append(answer)
+
+print(answerdict)
+
+cnt = 1;
+pages[-1].click()
